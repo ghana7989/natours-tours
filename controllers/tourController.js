5c5e469a378a4c86f4fe7d684d8472d16c90c386
ghana7989 <ghana.chindukuri@gmail.com>
2020-10-07 09:39:09 +0530

    dd

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,9 +1,11 @@
 const Tour = require("../models/tourModel")
 const APIFeatures = require("../utils/apiFeatures")
+
+
 exports.aliasTopFiveTours = (req, res, next) => {
     req.query.limit = 5;
     req.query.sort = "-ratingsAverage price";
-    req.query.fields = "name,price,ratingsAverage,summary,difficulty";
+    req.query.fields = "name,price,ratingsAverage,summary,difficulty,duration";
     next();
 }
 
--- models/tourModel.js
+++ models/tourModel.js
@@ -1,12 +1,21 @@
 const mongoose = require('mongoose');
-var slugify = require('slugify')
+var slugify = require('slugify');
+const validator = require('validator');
 
 const toursSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, "A Tour must have a name"],
         unique: true,
-        trim: true
+        trim: true,
+        maxlength: [30, "A Tour must have a name of less than 30 characters"],
+        minlength: [10, "A Tour must have a name of less than 10 characters"],
+        validate: {
+            validator: function (val) {
+                return validator.isAlpha(val.replace(/ /g, ""))
+            },
+            message: "Tour Name must not contain Numbers or special characters"
+        }
     },
     duration: {
         type: Number,
@@ -18,11 +27,17 @@ const toursSchema = new mongoose.Schema({
     },
     difficulty: {
         type: String,
-        required: [true, "A Tour must have a Difficulty Mentioned"]
+        required: [true, "A Tour must have a Difficulty Mentioned"],
+        enum: {
+            values: ["easy", "medium", "difficult"],
+            message: "Difficulty is either easy or medium or difficult"
+        }
     },
     ratingsAverage: {
         type: Number,
         default: 3,
+        min: [1, "Ratings must be greater than 1.0"],
+        max: [5, "Ratings must be lesser than 5.0"]
     },
     ratingsQuantity: {
         type: Number,
@@ -34,6 +49,14 @@ const toursSchema = new mongoose.Schema({
     },
     priceDiscount: {
         type: Number,
+        validate: {
+            validator: function (val) {
+                return val < this.price
+            },
+            message: "Discount price ({VAL}) should be less than original price"
+        }
+
+
     },
     summary: {
         type: String,
@@ -55,7 +78,10 @@ const toursSchema = new mongoose.Schema({
         select: false
     },
     startDates: [Date],
-
+    secretTour: {
+        type: Boolean,
+        default: false
+    },
 }, {
     toJSON: { virtuals: true },
     toObject: { virtuals: true }
@@ -67,12 +93,34 @@ toursSchema.virtual("tourDurationInWeeks").get(function () {
 toursSchema.pre("save", function (next) {
     this.slug = slugify(this.name, { lower: true })
     next()
-}) 
+})
 // This Document Middleware will run after all the pre middlewares ran
 // toursSchema.post("save", function (doc, next) {
 //     console.log(doc);
 //     next()
 // })
+
+// QUERY MIDDLEWARE
+
+toursSchema.pre(/^find/, function (next) {
+    this.find({ secretTour: { $ne: true } })
+    this.start = Date.now()
+    next();
+});
+toursSchema.post(/^find/, function (docs, next) {
+    console.log(`Query Took ${Date.now() - this.start} milliseconds`);
+    next();
+})
+
+// AGGREGATION MIDDLEWARE
+toursSchema.pre("aggregate", function (next) {
+    this.pipeline().unshift({ $match: { secretTour: { $ne: true } } })
+    next()
+})
+toursSchema.post("aggregate", function (doc, next) {
+    console.log(doc);
+    next()
+})
 const Tour = mongoose.model("Tour", toursSchema)
 
 module.exports = Tour
